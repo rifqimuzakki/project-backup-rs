9e79f61f821f2b78e2c40bc91a1606f08348657b
Rifqi <rifqimuzakki94@gmail.com>
2026-05-26 12:23:56 +0700

    fix: perpanjang masa simpan backup dari 7 ke 30 hari

--- backup.sh
+++ backup.sh
@@ -10,7 +10,7 @@
 FOLDER_SUMBER="/home/donny/belajar-devops/project-backup/data"
 FOLDER_BACKUP="/home/donny/belajar-devops/project-backup/backup"
 FOLDER_LOG="/home/donny/belajar-devops/project-backup/logs"
-SIMPAN_BERAPA_HARI=7
+SIMPAN_BERAPA_HARI=30
 
 # --- VARIABEL OTOMATIS ---
 TANGGAL=$(date +"%Y-%m-%d_%H-%M-%S")
